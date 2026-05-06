f0bd6bd0826b25169ca454ce20b48626ed446700
Emauay Palmer <emaujay.palmer@student.swic.edu>
2026-05-06 01:21:59 -0500

    feat:handle errors with resp.ok

--- src/main.js
+++ src/main.js
@@ -5,3 +5,17 @@ async function getPlanet(id) {
 }
 
 getPlanet(1);
+
+async function getPlanet(id) {
+  const resp = await fetch(`https://swapi.info/api/planets/${id}`);
+
+  if (!resp.ok) {
+    console.error(`Request failed: ${resp.status}`);
+    return;
+  }
+
+  const data = await resp.json();
+  console.info(data);
+}
+
+getPlanet(999);
